25c824be6c67c32463b5c9d809b84a3e3180c66a
Diko Tech Slave <diko316@gmail.com>
2017-03-12 00:15:32 +0800

    created tokenizer. not yet finalized parser.

--- src/eval.js
+++ src/eval.js
@@ -1,13 +1,14 @@
 'use strict';
 
 var LIBCORE = require("libcore"),
-    STREAM_PIPE = /|(?!|)/g;
+    IDENTIFIER_RE = /^[a-zA-Z\$\_][a-zA-Z\$\_0-9]*$/,
+    TOKEN = require("./eval/token.json");
 
 function interpolate(expr) {
     var core = LIBCORE,
         Fn = Function,
         body = '';
-        
+    
     if (core.string(expr)) {
         
         body = ([
@@ -24,11 +25,105 @@ function interpolate(expr) {
 
 function transpile(expr) {
     
+    var len = expr.length,
+        token = tokenize(expr, 0, len),
+        stackValue = null,
+        sl = 0,
+        pl = 0,
+        stack = [],
+        parseTree = [];
+    var id, str;
+    
+    console.log('expr: ', expr);
+    
+    for (; token;) {
+        //console.log('token ', token);
+        id = token[0];
+        str = token[1];
+        
+        switch (id) {
+        case ' ': break; // ignore whitespace
+        
+        // operands
+        case 'integer':
+        case 'float':
+        case 'string':
+        case 'identifier':
+            parseTree[pl++] = [id, str];
+            break;
+        
+        // operator precedence
+        case '-':
+        case '+':
+            
+            break;
+        
+        default:
+            console.log('uncaught ', token);
+        }
+
+        token = tokenize(expr, token[2], len);
+
+    }
     
     
     
+}
+
+function tokenize(expr, from, len) {
+    var l = Math.max(len - from, 0),
+        c = from,
+        next = from + 1,
+        map = TOKEN.state,
+        ends = TOKEN.token,
+        identifierRe = IDENTIFIER_RE,
+        state = 'start',
+        current = map.start,
+        token = null,
+        proceed = true;
+    var chr, catchAll, contains, not, notChr, buffered;
     
+    for (; l--;) {
+       
+        chr = expr.charAt(c++);
+        notChr = '!' + chr;
+        catchAll = current.catchAll;
+        contains = chr in current;
+        not = notChr in current;
+        
+        // dont buffer
+        if (not || (!contains && !catchAll)) {
+            buffered = false;
+            proceed = false;
+        }
+        else {
+            state = contains ?
+                        current[chr] : catchAll;
+            current = map[state];
+            buffered = true;
+        }
+        
+        if (state in ends) {
+            token = ends[state];
+            next = buffered ? c : c - 1;
+            if (token === 'extra' &&
+                identifierRe.test(expr.substring(from, next))) {
+                token = 'identifier';
+                
+                if (expr.charAt(next) === '(') {
+                    token = 'function';
+                    next++;
+                }
+            }
+        }
+        
+        if (!proceed) {
+            break;
+        }
+        
+    }
     
+    return token && [token, expr.substring(from, next), next];
 }
 
 
--- src/index.js
+++ src/index.js
@@ -5,20 +5,20 @@ var WORKFLOW, COMPONENT;
 
 // from here it is now safe to load browser modules
 if (LIBDOM.info.browser) {
-    WORKFLOW = require("./workflow.js");
-    
-    // register components
-    COMPONENT = require("./component.js");
-    
-    COMPONENT.register('application',
-                       require("./component/application.js"));
-    
-    
+    //WORKFLOW = require("./workflow.js");
+    //
+    //// register components
+    //COMPONENT = require("./component.js");
+    //
+    //COMPONENT.register('application',
+    //                   require("./component/application.js"));
     
     
     
+    var ev = require("./eval.js");
+    ev.interpolate("buang()");
     
-    WORKFLOW.start();
+    //WORKFLOW.start();
     
     
     
